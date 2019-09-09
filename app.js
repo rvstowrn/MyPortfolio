7831383989518982e1d83be766b78aaa96ef208f
Rishabh Verma <rvstowrn@gmail.com>
2019-09-09 15:11:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+$(function(){
+	$('.sidenav').sidenav();
+	$('.modal').modal();
+	$('.materialboxed').materialbox();
+	$('.parallax').parallax();
+	$('.tabs').tabs();
+	$('.datepicker').datepicker({
+		
+	});
+});
