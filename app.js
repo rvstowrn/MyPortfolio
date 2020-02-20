914a6ecb2b1fcff8e2aba59c23f13d5a449e6eb5
rvstowrn <rvstowrn@gmail.com>
2020-02-20 20:20:11 +0530

    dark mode update

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ $(function(){
 	});
 	$('.scrollspy').scrollSpy();
 });
+
+let toggle=1;
+function toggler(){
+	if(toggle)
+		darkMode();
+	else
+		lightMode();
+	toggle=!toggle;
+}
+
+function darkMode(){
+	$('#intro>div, #skills>div, #work>div>div>div, .tabs,form').css({
+	   'background' : '#333',
+	   'color' : '#fff',
+	   'box-shadow': '0 -2px 10px rgba(0, 0, 0, 1)'
+	});
+	$('body').css({
+	   'background' : '#121212',
+	   'color' : '#fff'
+	});
+	$('#intro').css({
+	   'background' : '#1A1A1A',
+	   'color' : '#fff'
+	});
+}
+function lightMode(){
+	$("#intro>div, #skills>div, #work>div>div>div, .tabs, form, body, #intro").removeAttr("style");
+}
